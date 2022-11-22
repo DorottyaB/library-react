53d74d77d639cadfe1f25ba89f7b61bd4206edd4
DorottyaB <dorottyabalogh89@gmail.com>
2022-11-22 15:11:43 +0100

    fix button padding

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,11 +1,12 @@
 import './button.css';
 
 const Button = ({ value, handleClick, color }) => {
-  const read = value === 'Read' ? 'read' : value === 'Not Read' ? 'not-read' : 'white';
+  const read = value === 'Read' ? 'read' : value === 'Not Read' ? 'not-read' : '';
   const type = color === 'primary' ? 'btn-primary' : 'btn-secondary';
+  const main = value === 'ADD NEW BOOK' ? 'btn-main' : '';
 
   return (
-    <button onClick={handleClick} color={color} className={`${read} ${type}`}>
+    <button onClick={handleClick} color={color} className={`${read} ${type} ${main}`}>
       {value}
     </button>
   );
