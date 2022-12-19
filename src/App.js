b8530a618045657503d3ca7029d116fa82a834bf
DorottyaB <dorottyabalogh89@gmail.com>
2022-12-19 18:14:09 +0100

    change file path

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import Header from './components/header/Header';
 import BookList from './components/book-list/BookList';
-import BookContextProvider from './context/BookContext';
-import FormContextProvider from './context/FormContext';
+import BookContextProvider from './context/bookContext';
+import FormContextProvider from './context/formContext';
 import ShowForm from './components/show-form/ShowForm';
 import Footer from './components/footer/Footer';
 
